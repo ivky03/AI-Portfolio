497bbc707b92604fc00623a17fa8220c59d8df97
ivky03 <kvk1011@gmail.com>
2025-03-06 18:57:51 -0700

    Disabled ESLint during build

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // ✅ This disables ESLint checks during build
+    },
+  };
+  
+  module.exports = nextConfig;
+  
